23ba0a849a8268f8bfeaa6e079c3cc278ace0533
Frangu Vlad <contact@kingdgrizzle.ml>
2018-03-19 15:01:39 +0200

    :art: :bug: Fixed bugs and refactor code Fix bug where Client#shard is defined in Commando / pm2 / nodemon env and shardCount turns to 0 eslint some more code

--- src/SendData.js
+++ src/SendData.js
@@ -23,12 +23,12 @@ module.exports = (client, token, DISCORDIE, paramName) => {
 		if (!DIO) {
 			if (client.shards && client.shards.size) {
 				// This is dirty, but w/e
+				shardCount = client.options.maxShards;
 				if (client.options.firstShardID === client.options.lastShardID) {
 					shardID = client.options.firstShardID;
 				} else {
 					ERIS = true;
 				}
-				shardCount = client.options.maxShards;
 			} else if (client.shard) {
 				shardID = client.shard.id;
 				shardCount = client.shard.count;
@@ -38,28 +38,21 @@ module.exports = (client, token, DISCORDIE, paramName) => {
 			}
 		}
 		totalGuilds = client.servers ? Object.keys(client.servers).length : client.guilds.size;
-		if (!ERIS) postData(shardID, shardCount, totalGuilds, token, client, false, paramName);
-		else {
+		if (!ERIS) {
+			postData(shardID, shardCount || 1, totalGuilds, token, client, false, paramName);
+		} else {
 			client.shards.forEach(s => {
-				const totalGuilds = client.guilds.filter(g => g.shard.id === s.id).length; // Thank Vlag for block-scoped variables
-				postData(s.id, shardCount, totalGuilds, token, client, false, paramName);
+				const totalGuildsOnShard = client.guilds.filter(g => g.shard.id === s.id).length;
+				postData(s.id, shardCount, totalGuildsOnShard, token, client, false, paramName);
 			});
 		}
 	}
 };
 
 function postData (shardID, shardCount, guilds, token, client, discordIE, paramName) {
-	if (shardID === null && shardCount === null) {
-		request.post(API_URL.replace("{id}", client.user.id))
-			.set("Authorization", token)
-			.send({ server_count: Number(guilds) })
-			.then(() => { client[paramName].emit("posted"); })
-			.catch(e => { client[paramName].emit("error", e); });
-	} else {
-		request.post(API_URL.replace("{id}", discordIE ? client.User.id : client.id ? client.id : client.user.id))
-			.set("Authorization", token)
-			.send({ server_count: Number(guilds), shard_id: Number(shardID), shard_count: Number(shardCount) })
-			.then(() => { client[paramName].emit("posted"); })
-			.catch(e => { client[paramName].emit("error", e); });
-	}
+	request.post(API_URL.replace("{id}", discordIE ? client.User.id : client.id ? client.id : client.user.id))
+		.set("Authorization", token)
+		.send({ server_count: Number(guilds), shard_id: Number(shardID), shard_count: Number(shardCount || 1) })
+		.then(() => { client[paramName].emit("posted"); })
+		.catch(e => { client[paramName].emit("error", e); });
 }
